6ad81e0c812ec579a4ab2efba8efc69dc95df4a4
Minhazul Abedin <minhazulabedin44@gmail.com>
2024-08-14 11:14:56 +0600

    no changes

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,12 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config')
- 
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+
 const config = {
-  resolver: {
-    assetExts: ['tflite', ...getDefaultConfig(__dirname)?.resolver?.assetExts],
+  transformer: {
+    unstable_allowRequireContext: true,
   },
-}
- 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config)
+  resolver: {
+    assetExts: ["tflite", ...getDefaultConfig(__dirname)?.resolver?.assetExts],
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
